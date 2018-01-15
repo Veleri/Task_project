428192cb17cbab7498c36414f79c12369b624844
Veleri <R.Veleri@yandex.ua>
2018-01-15 23:12:15 +0200

    Move calculateDiscount to ItemType

--- src/main/java/ItemType.java
+++ src/main/java/ItemType.java
@@ -27,4 +27,30 @@ public enum ItemType {
         }
         throw new IllegalArgumentException("Invalid parameter value");
     }
+
+
+    /**
+     * Calculates item's discount.
+     * For NEW item discount is 0%;
+     * For SECOND_FREE item discount is 50% if quantity > 1
+     * For SALE item discount is 70%
+     * For each full 10 not NEW items item gets additional 1% discount,
+     * but not more than 80% total
+     */
+    public static int calculateDiscount(ItemType type, int quantity) {
+        int discount = 0;
+        if(type.equals(ItemType.SECOND_FREE) && quantity > 1){
+            discount = 50;
+        }
+        if(type.equals(ItemType.SALE)){
+            discount = 70;
+        }
+        if(!type.equals(ItemType.NEW) &&  quantity >= 10 ){
+            discount += quantity / 10;
+            if(discount > 80){
+                discount = 80;
+            }
+        }
+        return  discount;
+    }
 }
